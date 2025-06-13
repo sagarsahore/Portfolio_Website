460593f435bc273c8e1a8c9c4c94a835cee2e618
Sagar Sahore <65637133+sagarsahore@users.noreply.github.com>
2025-06-13 13:35:07 +1200

    Add expandable projects section

---
+++ assets/scripts/show-more-projects.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    const btn = $('#toggle-projects');
+    const projects = $('#additional-projects');
+
+    btn.on('click', function(){
+        // Toggle additional projects with a sliding animation
+        projects.slideToggle(400);
+        const expanded = btn.attr('data-expanded') === 'true';
+        if(expanded){
+            btn.attr('data-expanded','false');
+            btn.find('.vlt-btn__text').text('Show More Projects');
+        } else {
+            btn.attr('data-expanded','true');
+            btn.find('.vlt-btn__text').text('Show Less Projects');
+        }
+    });
+});
