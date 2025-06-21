2bf565bf49636367bb4f1d2fc04004672e3c3245
Sagar Sahore <65637133+sagarsahore@users.noreply.github.com>
2025-06-21 16:32:00 +1200

    Add independent timeline sliders

--- app/controllers/_controller-timeline-slider.js
+++ app/controllers/_controller-timeline-slider.js
@@ -11,31 +11,34 @@
 			if (typeof Swiper == 'undefined') {
 				return;
 			}
-			var el = $('.vlt-timeline-slider .swiper-container');
-			el.each(function () {
-				var $this = $(this);
-				$this.find('.swiper-wrapper > *').wrap('<div class="swiper-slide">');
-				new Swiper(this, {
-					speed: 1000,
-					spaceBetween: 0,
-					grabCursor: true,
-					slidesPerView: 1,
-					navigation: {
-						nextEl: $('.vlt-timeline-slider-controls .next'),
-						prevEl: $('.vlt-timeline-slider-controls .prev'),
-					},
-					pagination: {
-						el: $('.vlt-timeline-slider-controls .pagination'),
-						clickable: false,
-						type: 'fraction',
-						renderFraction: function (currentClass, totalClass) {
-							return '<span class="' + currentClass + '"></span>' +
-								'<span class="sep">/</span>' +
-								'<span class="' + totalClass + '"></span>';
-						}
-					}
-				});
-			});
+                       var el = $('.vlt-timeline-slider .swiper-container');
+                       var controls = $('.vlt-timeline-slider-controls');
+                       el.each(function (index) {
+                                var $this = $(this);
+                                $this.find('.swiper-wrapper > *').wrap('<div class="swiper-slide">');
+
+                                var $ctrl = controls.eq(index);
+                                new Swiper(this, {
+                                        speed: 1000,
+                                        spaceBetween: 0,
+                                        grabCursor: true,
+                                        slidesPerView: 1,
+                                        navigation: {
+                                                nextEl: $ctrl.find('.next')[0],
+                                                prevEl: $ctrl.find('.prev')[0]
+                                        },
+                                        pagination: {
+                                                el: $ctrl.find('.pagination')[0],
+                                                clickable: false,
+                                                type: 'fraction',
+                                                renderFraction: function (currentClass, totalClass) {
+                                                        return '<span class="' + currentClass + '"></span>' +
+                                                                '<span class="sep">/</span>' +
+                                                                '<span class="' + totalClass + '"></span>';
+                                                }
+                                        }
+                                });
+                       });
 
 		}
 	};
